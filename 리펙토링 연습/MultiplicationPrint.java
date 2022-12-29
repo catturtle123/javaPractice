6bdb2beb31bac07729c840e8e45dd8ccf98f01fa
jamey <101050942+catturtle123@users.noreply.github.com>
2022-12-29 22:16:01 +0900

    Create MultiplicationPrint.java

---
+++ 리펙토링 연습/MultiplicationPrint.java
@@ -0,0 +1,13 @@
+package com.pratice1;
+
+public class MultiplicationPrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MultiplicationTable multiplicationTable = new MultiplicationTable();
+		// multiplicationTable.print();
+		multiplicationTable.print(6);
+		multiplicationTable.print(6, 11, 20);
+	}
+
+}
