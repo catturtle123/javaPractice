e9fb8eeb003abcbe3fe77ce6ac4952e635f857f6
jamey <101050942+catturtle123@users.noreply.github.com>
2022-12-30 16:11:19 +0900

    Create MotorBike.java

---
+++ 인스턴스 만들기 연습/MotorBike.java
@@ -0,0 +1,17 @@
+package practice2;
+
+public class MotorBikePrint {
+
+	public static void main(String[] args) {
+		MotorBike ducati = new MotorBike();
+		MotorBike honda = new MotorBike();
+
+		ducati.speed = 100;
+		honda.speed = 80;
+
+		ducati.speed = 20;
+		honda.speed = 0;
+
+	}
+
+}
