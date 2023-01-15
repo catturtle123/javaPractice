36e4ae209807156312995905397e85e6dd374c9c
jamey <101050942+catturtle123@users.noreply.github.com>
2023-01-15 23:35:58 +0900

    Create WhileRunner.java

---
+++ while문 연습/WhileRunner.java
@@ -0,0 +1,14 @@
+package whileLoop;
+
+public class WhileRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WhileNumberPlayer player = new WhileNumberPlayer(30);
+
+		player.printSquaresUptoLimit();
+
+		player.printCubesUptoLimit();
+	}
+
+}
