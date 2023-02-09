bc9a3d8b0f25220e3259a71abef891cca50e2c1d
jamey <101050942+catturtle123@users.noreply.github.com>
2023-02-09 18:56:08 +0900

    Create MarioGame.java

---
+++ 인터페이스 연습/MarioGame.java
@@ -0,0 +1,28 @@
+package game1;
+
+public class MarioGame implements GamingConsole {
+	@Override
+	public void up() {
+		// TODO Auto-generated method stub
+		System.out.println("up");
+	}
+
+	@Override
+	public void down() {
+		// TODO Auto-generated method stub
+		System.out.println("down");
+	}
+
+	@Override
+	public void right() {
+		// TODO Auto-generated method stub
+		System.out.println("right");
+	}
+
+	@Override
+	public void left() {
+		// TODO Auto-generated method stub
+		System.out.println("left");
+	}
+
+}
