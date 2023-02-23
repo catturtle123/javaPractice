5b03caa62842878d057a52bd2eabc742ba6bb988
jamey <101050942+catturtle123@users.noreply.github.com>
2023-02-24 03:18:59 +0900

    Create StaticImport.java

---
+++ static import 연습/StaticImport.java
@@ -0,0 +1,18 @@
+package tipAndTricks;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import static java.util.Collections.*;
+import static java.lang.System.out;
+
+
+public class StaticImport {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BigDecimal db = null;
+		sort(new ArrayList<String>());
+		out.println("hi");
+	}
+
+}
