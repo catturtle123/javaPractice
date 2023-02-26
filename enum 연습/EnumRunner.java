7b2dd95801c56c79b912abeb2a03d345f2b65700
jamey <101050942+catturtle123@users.noreply.github.com>
2023-02-27 00:19:02 +0900

    Update EnumRunner.java

--- enum 연습/EnumRunner.java
+++ enum 연습/EnumRunner.java
@@ -2,10 +2,6 @@ package tipAndTricks;
 
 import java.util.Arrays;
 
-enum Season{
-	WINTER, SUMMER, FALL, SPRING
-}
-
 public class EnumRunner {
 	public static void main(String[] args) {
 		Season season1 = Season.FALL;
@@ -13,6 +9,7 @@ public class EnumRunner {
 	
 		System.out.println(season1);
 		System.out.println(season1.ordinal());
+		System.out.println(season1.getValue());
 		System.out.println(Arrays.toString(Season.values()));
 		
 	}
